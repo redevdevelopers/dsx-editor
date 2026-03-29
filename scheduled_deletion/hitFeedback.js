18a0dd357c76fe6c0892add5fb17921e138f2b72
Kyrilla@Redevon <redevcommunity@gmail.com>
2026-03-30 04:34:04 +0800

    refactor: remove unused files and associated references

--- scheduled_deletion/hitFeedback.js
+++
@@ -1,7 +0,0 @@
-// This file is not used in the editor
-// It exists only to prevent 404 errors from cached browser imports
-// The editor does not need hit feedback functionality
-
-export function showHitFeedback() {
-    // No-op in editor
-}
